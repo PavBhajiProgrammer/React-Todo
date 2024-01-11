1e60599d0864f8943af2515576b562096256e7fb
pavbhajiprogrammer <pavbhajiprogrammer@gmail.com>
2024-01-11 16:33:16 +0530

    completed project

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -1,23 +1,66 @@
-import { useState } from "react";
-import react from 'react';
+// import { useState } from "react";
 
-const Todo = () => {
+// const Todo = () => {
 
-  const [task, setTasks] = useState(null);
+//   const [task, setTasks] = useState(null);
 
-  const handleInput = (e) => {
-    newTasks = {
-        body:e.target.value,
-    }
+//   const handleInput = (e) => {
+//     newTasks = {
+//         body:e.target.value,
+//     }
 
-    setTasks(...task, newTasks);
-  }
+//     setTasks(...task, newTasks);
+//   }
+//   return (
+//     <div>
+//       <input onClick={handleInput} placeholder="Enter task..."  />
+//       {task}
+//     </div>
+//   )
+// }
+
+// export default Todo
+
+// =============================== 2nd approach
+
+import React from 'react'
+import { useState } from 'react'
+
+function TodoList () {
+  const [todos, setTodos] = useState([])
+  const [inputValue, setInputValue] = useState('')
+
+function handleChange(e){
+  setInputValue(e.target.value)
+}
+
+function handleSubmit(e){
+  e.preventDefault()
+  setTodos([...todos, inputValue])
+  setInputValue('')
+}
+
+function handleDelete(index){
+  const newTodos = [...todos]
+  newTodos.splice(index, 1)
+  setTodos(newTodos)
+}
   return (
     <div>
-      <input onClick={handleInput} placeholder="Enter task..."  />
-      {task}
+      <h1>Todo List</h1>
+      <form>
+        <input type='text' value={inputValue} onChange={handleChange}/>
+        <button onClick={handleSubmit}>Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}
+          <button onClick={() =>handleDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
 
-export default Todo
+export default TodoList;
